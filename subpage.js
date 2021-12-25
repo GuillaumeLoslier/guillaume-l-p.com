bf3113f9297867b250e058d116fe2948e950ada2
Guillaume Loslier-Pinard <80226854+GuillaumeLoslier@users.noreply.github.com>
2021-12-25 11:58:48 +0100

    Update subpage.js

--- subpage.js
+++ subpage.js
@@ -1,3 +1,41 @@
+
+const items = document.querySelector(".items");
+const dots = document.querySelectorAll(".dot");
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const total = items.children.length - 1;
+const active = "active";
+let current = 0;
+
+const setActiveDot = () => {
+  dots.forEach((button, i) => {
+    i === current
+      ? button.classList.add(active)
+      : button.classList.remove(active);
+  });
+};
+
+const scrollToCurrent = () => {
+  items.style.transform = `translateX(${current * -100}%`;
+  setActiveDot();
+};
+
+const scrollPrev = () => {
+  if (current === 0) return;
+  current--;
+  scrollToCurrent();
+};
+
+const scrollNext = () => {
+  if (current === total) return;
+  current++;
+  scrollToCurrent();
+};
+
+prev.addEventListener("click", scrollPrev);
+next.addEventListener("click", scrollNext);
+
+
 // forEach function
 var forEach = function (array, callback, scope) {
     for (var i = 0; i < array.length; i++) {
