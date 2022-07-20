cb0ffc5762bd4fb1b2c0fd0e2bdb643ea657849f
GuillaumeLoslier <gloslierpinard@gmail.com>
2022-07-20 16:17:51 -0400

    Added javascript interaction

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,14 +1,553 @@
+parcelRequire = (function (e, r, t, n) {
+  var i,
+    o = "function" == typeof parcelRequire && parcelRequire,
+    u = "function" == typeof require && require;
+  function f(t, n) {
+    if (!r[t]) {
+      if (!e[t]) {
+        var i = "function" == typeof parcelRequire && parcelRequire;
+        if (!n && i) return i(t, !0);
+        if (o) return o(t, !0);
+        if (u && "string" == typeof t) return u(t);
+        var c = new Error("Cannot find module '" + t + "'");
+        throw ((c.code = "MODULE_NOT_FOUND"), c);
+      }
+      (p.resolve = function (r) {
+        return e[t][1][r] || r;
+      }),
+        (p.cache = {});
+      var l = (r[t] = new f.Module(t));
+      e[t][0].call(l.exports, p, l, l.exports, this);
+    }
+    return r[t].exports;
+    function p(e) {
+      return f(p.resolve(e));
+    }
+  }
+  (f.isParcelRequire = !0),
+    (f.Module = function (e) {
+      (this.id = e), (this.bundle = f), (this.exports = {});
+    }),
+    (f.modules = e),
+    (f.cache = r),
+    (f.parent = o),
+    (f.register = function (r, t) {
+      e[r] = [
+        function (e, r) {
+          r.exports = t;
+        },
+        {},
+      ];
+    });
+  for (var c = 0; c < t.length; c++)
+    try {
+      f(t[c]);
+    } catch (e) {
+      i || (i = e);
+    }
+  if (t.length) {
+    var l = f(t[t.length - 1]);
+    "object" == typeof exports && "undefined" != typeof module
+      ? (module.exports = l)
+      : "function" == typeof define && define.amd
+      ? define(function () {
+          return l;
+        })
+      : n && (this[n] = l);
+  }
+  if (((parcelRequire = f), i)) throw i;
+  return f;
+})(
+  {
+    KZrH: [
+      function (require, module, exports) {},
+      {
+        "./../fonts/metro-nova/MetroNovaRegular.eot": [
+          ["MetroNovaRegular.c844446a.eot", "xsPp"],
+          "xsPp",
+        ],
+        "./../fonts/metro-nova/MetroNovaRegular.woff2": [
+          ["MetroNovaRegular.4092cafd.woff2", "dVn8"],
+          "dVn8",
+        ],
+        "./../fonts/metro-nova/MetroNovaRegular.woff": [
+          ["MetroNovaRegular.7d2322d0.woff", "u2a4"],
+          "u2a4",
+        ],
+        "./../fonts/metro-nova/MetroNovaRegular.ttf": [
+          ["MetroNovaRegular.709ef096.ttf", "pNQZ"],
+          "pNQZ",
+        ],
+        "./../fonts/metro-nova/MetroNovaRegularItalic.eot": [
+          ["MetroNovaRegularItalic.dfc7bb5d.eot", "zcKW"],
+          "zcKW",
+        ],
+        "./../fonts/metro-nova/MetroNovaRegularItalic.woff2": [
+          ["MetroNovaRegularItalic.44b581dc.woff2", "rBTZ"],
+          "rBTZ",
+        ],
+        "./../fonts/metro-nova/MetroNovaRegularItalic.woff": [
+          ["MetroNovaRegularItalic.be1df5d5.woff", "nN2j"],
+          "nN2j",
+        ],
+        "./../fonts/metro-nova/MetroNovaRegularItalic.ttf": [
+          ["MetroNovaRegularItalic.4d944fb3.ttf", "oI4K"],
+          "oI4K",
+        ],
+        "./../fonts/metro-nova/MetroNovaBold.eot": [
+          ["MetroNovaBold.47f1913f.eot", "B8I6"],
+          "B8I6",
+        ],
+        "./../fonts/metro-nova/MetroNovaBold.woff2": [
+          ["MetroNovaBold.8fba494b.woff2", "ISP1"],
+          "ISP1",
+        ],
+        "./../fonts/metro-nova/MetroNovaBold.woff": [
+          ["MetroNovaBold.7ab1c80f.woff", "k4xA"],
+          "k4xA",
+        ],
+        "./../fonts/metro-nova/MetroNovaBold.ttf": [
+          ["MetroNovaBold.1c4bf6cc.ttf", "mFsI"],
+          "mFsI",
+        ],
+        "./../fonts/metro-nova/MetroNovaBoldItalic.eot": [
+          ["MetroNovaBoldItalic.76e0fb33.eot", "sVZN"],
+          "sVZN",
+        ],
+        "./../fonts/metro-nova/MetroNovaBoldItalic.woff2": [
+          ["MetroNovaBoldItalic.9fbe74c3.woff2", "h6vn"],
+          "h6vn",
+        ],
+        "./../fonts/metro-nova/MetroNovaBoldItalic.woff": [
+          ["MetroNovaBoldItalic.f8005fa0.woff", "jkVs"],
+          "jkVs",
+        ],
+        "./../fonts/metro-nova/MetroNovaBoldItalic.ttf": [
+          ["MetroNovaBoldItalic.7cd7f899.ttf", "QEAo"],
+          "QEAo",
+        ],
+        "./../fonts/metro-nova/MetroNovaMedium.eot": [
+          ["MetroNovaMedium.6930c507.eot", "WCJW"],
+          "WCJW",
+        ],
+        "./../fonts/metro-nova/MetroNovaMedium.woff2": [
+          ["MetroNovaMedium.355451cd.woff2", "xaqn"],
+          "xaqn",
+        ],
+        "./../fonts/metro-nova/MetroNovaMedium.woff": [
+          ["MetroNovaMedium.7289285f.woff", "TlwJ"],
+          "TlwJ",
+        ],
+        "./../fonts/metro-nova/MetroNovaMedium.ttf": [
+          ["MetroNovaMedium.4f63b6e6.ttf", "VgK3"],
+          "VgK3",
+        ],
+        "./../fonts/metro-nova/MetroNovaMediumItalic.eot": [
+          ["MetroNovaMediumItalic.c33d3181.eot", "PlHc"],
+          "PlHc",
+        ],
+        "./../fonts/metro-nova/MetroNovaMediumItalic.woff2": [
+          ["MetroNovaMediumItalic.d4c7759d.woff2", "tuFV"],
+          "tuFV",
+        ],
+        "./../fonts/metro-nova/MetroNovaMediumItalic.woff": [
+          ["MetroNovaMediumItalic.e5dc3841.woff", "GAQ1"],
+          "GAQ1",
+        ],
+        "./../fonts/metro-nova/MetroNovaMediumItalic.ttf": [
+          ["MetroNovaMediumItalic.c0d6a3f0.ttf", "yj7f"],
+          "yj7f",
+        ],
+        "./../fonts/metro-nova/MetroNovaBlack.eot": [
+          ["MetroNovaBlack.618f45d1.eot", "YhZ6"],
+          "YhZ6",
+        ],
+        "./../fonts/metro-nova/MetroNovaBlack.woff2": [
+          ["MetroNovaBlack.0c7ae274.woff2", "mAw7"],
+          "mAw7",
+        ],
+        "./../fonts/metro-nova/MetroNovaBlack.woff": [
+          ["MetroNovaBlack.c71d95f4.woff", "rFJa"],
+          "rFJa",
+        ],
+        "./../fonts/metro-nova/MetroNovaBlack.ttf": [
+          ["MetroNovaBlack.d5b4772c.ttf", "WIw8"],
+          "WIw8",
+        ],
+        "./../fonts/ia-writer-duo/iAWriterDuoV.ttf": [
+          ["iAWriterDuoV.a64532b5.ttf", "E7Ri"],
+          "E7Ri",
+        ],
+        "./../fonts/ia-writer-duo/iAWriterDuoV-Italic.ttf": [
+          ["iAWriterDuoV-Italic.e8d24af1.ttf", "f3JV"],
+          "f3JV",
+        ],
+        "./../fonts/ia-writer-duo/iAWriterDuoS-Regular.eot": [
+          ["iAWriterDuoS-Regular.9648b0f4.eot", "QOfH"],
+          "QOfH",
+        ],
+        "./../fonts/ia-writer-duo/iAWriterDuoS-Regular.woff2": [
+          ["iAWriterDuoS-Regular.2f10ffbb.woff2", "X0iX"],
+          "X0iX",
+        ],
+        "./../fonts/ia-writer-duo/iAWriterDuoS-Regular.woff": [
+          ["iAWriterDuoS-Regular.878a8e48.woff", "udMm"],
+          "udMm",
+        ],
+        "./../fonts/ia-writer-duo/iAWriterDuoS-Bold.eot": [
+          ["iAWriterDuoS-Bold.c7b10fa4.eot", "YU1K"],
+          "YU1K",
+        ],
+        "./../fonts/ia-writer-duo/iAWriterDuoS-Bold.woff2": [
+          ["iAWriterDuoS-Bold.45153b1b.woff2", "SNY8"],
+          "SNY8",
+        ],
+        "./../fonts/ia-writer-duo/iAWriterDuoS-Bold.woff": [
+          ["iAWriterDuoS-Bold.6f142337.woff", "e2B6"],
+          "e2B6",
+        ],
+        "./../fonts/ia-writer-duo/iAWriterDuoS-Italic.eot": [
+          ["iAWriterDuoS-Italic.34b7e29a.eot", "eQUR"],
+          "eQUR",
+        ],
+        "./../fonts/ia-writer-duo/iAWriterDuoS-Italic.woff2": [
+          ["iAWriterDuoS-Italic.6c3828b8.woff2", "SYER"],
+          "SYER",
+        ],
+        "./../fonts/ia-writer-duo/iAWriterDuoS-Italic.woff": [
+          ["iAWriterDuoS-Italic.307de27d.woff", "e3CY"],
+          "e3CY",
+        ],
+        "./../fonts/ia-writer-duo/iAWriterDuoS-BoldItalic.eot": [
+          ["iAWriterDuoS-BoldItalic.42ae3662.eot", "sAh7"],
+          "sAh7",
+        ],
+        "./../fonts/ia-writer-duo/iAWriterDuoS-BoldItalic.woff2": [
+          ["iAWriterDuoS-BoldItalic.f62e4cae.woff2", "YxAC"],
+          "YxAC",
+        ],
+        "./../fonts/ia-writer-duo/iAWriterDuoS-BoldItalic.woff": [
+          ["iAWriterDuoS-BoldItalic.6a529122.woff", "Mz8c"],
+          "Mz8c",
+        ],
+        "./../fonts/ibm-plex-mono/IBMPlexMono-Bold.eot": [
+          ["IBMPlexMono-Bold.e4025184.eot", "B0H0"],
+          "B0H0",
+        ],
+        "./../fonts/ibm-plex-mono/IBMPlexMono-Bold.woff2": [
+          ["IBMPlexMono-Bold.70e5efbb.woff2", "CiPi"],
+          "CiPi",
+        ],
+        "./../fonts/ibm-plex-mono/IBMPlexMono-Bold.woff": [
+          ["IBMPlexMono-Bold.a59dacbe.woff", "zAJn"],
+          "zAJn",
+        ],
+        "./../fonts/ibm-plex-mono/IBMPlexMono-Text.eot": [
+          ["IBMPlexMono-Text.fe3bd42b.eot", "y81n"],
+          "y81n",
+        ],
+        "./../fonts/ibm-plex-mono/IBMPlexMono-Text.woff2": [
+          ["IBMPlexMono-Text.b51b09b6.woff2", "lU1d"],
+          "lU1d",
+        ],
+        "./../fonts/ibm-plex-mono/IBMPlexMono-Text.woff": [
+          ["IBMPlexMono-Text.8ae5e870.woff", "fmEK"],
+          "fmEK",
+        ],
+        "./../fonts/pique/PiqueWeb.eot": [
+          ["PiqueWeb.02e82cba.eot", "cdER"],
+          "cdER",
+        ],
+        "./../fonts/pique/PiqueWeb.woff2": [
+          ["PiqueWeb.43635cd2.woff2", "yMlk"],
+          "yMlk",
+        ],
+        "./../fonts/pique/PiqueWeb.woff": [
+          ["PiqueWeb.f21d4dae.woff", "JpFj"],
+          "JpFj",
+        ],
+        "./../fonts/elena/ElenaWebBasicMedium.eot": [
+          ["ElenaWebBasicMedium.52b5bc69.eot", "LzWQ"],
+          "LzWQ",
+        ],
+        "./../fonts/elena/ElenaWebBasicMedium.woff2": [
+          ["ElenaWebBasicMedium.26258b41.woff2", "ZEVg"],
+          "ZEVg",
+        ],
+        "./../fonts/elena/ElenaWebBasicMedium.woff": [
+          ["ElenaWebBasicMedium.0d6190b2.woff", "o2zH"],
+          "o2zH",
+        ],
+        "./../fonts/canela/CanelaDeck-Bold-Web.eot": [
+          ["CanelaDeck-Bold-Web.0c92f0f3.eot", "iSDK"],
+          "iSDK",
+        ],
+        "./../fonts/canela/CanelaDeck-Bold-Web.woff2": [
+          ["CanelaDeck-Bold-Web.e08e6944.woff2", "vZMe"],
+          "vZMe",
+        ],
+        "./../fonts/canela/CanelaDeck-Bold-Web.woff": [
+          ["CanelaDeck-Bold-Web.0e1e706b.woff", "q89t"],
+          "q89t",
+        ],
+        "./../fonts/canela/CanelaText-RegularNo2-Web.eot": [
+          ["CanelaText-RegularNo2-Web.6a553a4b.eot", "px8R"],
+          "px8R",
+        ],
+        "./../fonts/canela/CanelaText-RegularNo2-Web.woff2": [
+          ["CanelaText-RegularNo2-Web.dec02107.woff2", "uNY8"],
+          "uNY8",
+        ],
+        "./../fonts/canela/CanelaText-RegularNo2-Web.woff": [
+          ["CanelaText-RegularNo2-Web.0e587a7c.woff", "mrMk"],
+          "mrMk",
+        ],
+      },
+    ],
+    Focm: [
+      function (require, module, exports) {
+        "use strict";
+        function t(t, e) {
+          var n =
+            ("undefined" != typeof Symbol && t[Symbol.iterator]) ||
+            t["@@iterator"];
+          if (!n) {
+            if (
+              Array.isArray(t) ||
+              (n = r(t)) ||
+              (e && t && "number" == typeof t.length)
+            ) {
+              n && (t = n);
+              var o = 0,
+                a = function () {};
+              return {
+                s: a,
+                n: function () {
+                  return o >= t.length
+                    ? { done: !0 }
+                    : { done: !1, value: t[o++] };
+                },
+                e: function (t) {
+                  throw t;
+                },
+                f: a,
+              };
+            }
+            throw new TypeError(
+              "Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."
+            );
+          }
+          var i,
+            s = !0,
+            c = !1;
+          return {
+            s: function () {
+              n = n.call(t);
+            },
+            n: function () {
+              var t = n.next();
+              return (s = t.done), t;
+            },
+            e: function (t) {
+              (c = !0), (i = t);
+            },
+            f: function () {
+              try {
+                s || null == n.return || n.return();
+              } finally {
+                if (c) throw i;
+              }
+            },
+          };
+        }
+        function e(t) {
+          return a(t) || o(t) || r(t) || n();
+        }
+        function n() {
+          throw new TypeError(
+            "Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."
+          );
+        }
+        function r(t, e) {
+          if (t) {
+            if ("string" == typeof t) return i(t, e);
+            var n = Object.prototype.toString.call(t).slice(8, -1);
+            return (
+              "Object" === n && t.constructor && (n = t.constructor.name),
+              "Map" === n || "Set" === n
+                ? Array.from(t)
+                : "Arguments" === n ||
+                  /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)
+                ? i(t, e)
+                : void 0
+            );
+          }
+        }
+        function o(t) {
+          if (
+            ("undefined" != typeof Symbol && null != t[Symbol.iterator]) ||
+            null != t["@@iterator"]
+          )
+            return Array.from(t);
+        }
+        function a(t) {
+          if (Array.isArray(t)) return i(t);
+        }
+        function i(t, e) {
+          (null == e || e > t.length) && (e = t.length);
+          for (var n = 0, r = new Array(e); n < e; n++) r[n] = t[n];
+          return r;
+        }
+        function s(t, e) {
+          if (!(t instanceof e))
+            throw new TypeError("Cannot call a class as a function");
+        }
+        function c(t, e) {
+          for (var n = 0; n < e.length; n++) {
+            var r = e[n];
+            (r.enumerable = r.enumerable || !1),
+              (r.configurable = !0),
+              "value" in r && (r.writable = !0),
+              Object.defineProperty(t, r.key, r);
+          }
+        }
+        function u(t, e, n) {
+          return e && c(t.prototype, e), n && c(t, n), t;
+        }
+        require("../Styles/main-style.css");
+        var l,
+          d = (function () {
+            function t() {
+              var e = this;
+              s(this, t),
+                (this.buttonClass = ".font-changer__button"),
+                (this.overlayClass = "font-changer__overlay"),
+                (this.overlayActiveClass = "font-changer__overlay--active"),
+                (this.buttons = document.querySelectorAll(this.buttonClass)),
+                (this.overlay = document.createElement("div")),
+                this.overlay.classList.add(this.overlayClass),
+                this.buttons.forEach(function (t) {
+                  t.addEventListener("click", function (t) {
+                    return e.handleFontChange(t);
+                  });
+                });
+            }
+            return (
+              u(t, [
+                {
+                  key: "handleFontChange",
+                  value: function (t) {
+                    var e = this.overlay,
+                      n = this.overlayActiveClass;
+                    document.body.querySelector(
+                      ".".concat(this.overlayClass)
+                    ) && e.classList.remove(n),
+                      document.body.appendChild(e),
+                      setTimeout(function () {
+                        e.classList.add(n);
+                      }, 200);
+                    e.addEventListener("transitionend", function r() {
+                      document.documentElement.setAttribute(
+                        "font",
+                        t.target.dataset.font
+                      ),
+                        e.classList.remove(n),
+                        e.removeEventListener("transitionend", r),
+                        setTimeout(function () {
+                          document.body.removeChild(e);
+                        }, 500);
+                    });
+                  },
+                },
+              ]),
+              t
+            );
+          })(),
+          f = (function () {
+            function t() {
+              var e = this;
+              s(this, t),
+                (this.dataSelector = "[data-guillaume]"),
+                (this.guillaumeActivators = document.querySelectorAll(
+                  "span".concat(this.dataSelector)
+                )),
+                (this.guillaumes = document.querySelectorAll(
+                  "img".concat(this.dataSelector)
+                )),
+                (this.inactiveClass = "intro__image--inactive"),
+                this.guillaumeActivators.forEach(function (t) {
+                  t.addEventListener("mouseenter", function (t) {
+                    e.handleguillaumeActivation(t);
+                  });
+                });
+            }
+            return (
+              u(t, [
+                {
+                  key: "handleguillaumeActivation",
+                  value: function (t) {
+                    var n = this,
+                      r = e(this.guillaumes).filter(function (e) {
+                        return e.dataset.guillaume === t.target.dataset.guillaume;
+                      })[0];
+                    this.guillaumes.forEach(function (t) {
+                      return t.classList.add(n.inactiveClass);
+                    }),
+                      r.classList.remove(this.inactiveClass);
+                  },
+                },
+              ]),
+              t
+            );
+          })(),
+          v = new f(),
+          h = new d(),
+          y = document.querySelectorAll(".post__content img"),
+          m = document.querySelector(".pop"),
+          b = t(y);
+        try {
+          var g = function () {
+            var t = l.value,
+              e = t.src,
+              n = t.alt || "";
+            t.onclick = function () {
+              var t = m.querySelector("img");
+              (t.src = e), (t.alt = n), m.classList.add("show");
+            };
+          };
+          for (b.s(); !(l = b.n()).done; ) g();
+        } catch (p) {
+          b.e(p);
+        } finally {
+          b.f();
+        }
+        (m.onclick = function () {
+          m.classList.remove("show");
+        }),
+          (document.onkeyup = function (t) {
+            "Escape" === (t = t || window.event).key &&
+              m.classList.remove("show");
+          });
+      },
+      { "../Styles/main-style.css": "KZrH" },
+    ],
+  },
+  {},
+  ["Focm"],
+  null
+);
+
 (function($, document) {
     
-      // get tallest tab__content element
-      let height = -1;
+   // get tallest tab__content element
+   let height = -1;
 
-		$('.tab__content').each(function() {
-			height = height > $(this).outerHeight() ? height : $(this).outerHeight();
-         $(this).css('position', 'absolute');
-		});
-      
-      // set height of tabs + top offset
-		$('[data-tabs]').css('min-height', height + 40 + 'px');
+   $('.tab__content').each(function() {
+      height = height > $(this).outerHeight() ? height : $(this).outerHeight();
+      $(this).css('position', 'absolute');
+   });
    
+   // set height of tabs + top offset
+   $('[data-tabs]').css('min-height', height + 40 + 'px');
+
 }(jQuery, document));
